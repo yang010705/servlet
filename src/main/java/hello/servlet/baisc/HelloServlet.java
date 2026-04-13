db9c6c2766cc553de897e8c94b92e5cb2dc72944
yang010705 <acs0705@naver.com>
2026-04-13 17:54:52 +0900

    yang

---
+++ src/main/java/hello/servlet/baisc/HelloServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.baisc;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
